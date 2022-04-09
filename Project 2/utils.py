602b97d1b856c36b2d32b8a5c17b9822d483f138
mbakal2 <mtbakall@gmail.com>
2022-04-09 17:34:05 -0500

    Updated utils
    Nireek fixed it

--- Project 2/utils.py
+++ Project 2/utils.py
@@ -6,35 +6,36 @@ import math
 
 def vec2hat(x):
     # Find x^
-    x_hat = vec2hat(np.array([[0,0,1]])) 
+    x_hat = np.array([[0,-x1[0],x2[0]],[x1[0],0,x3[0]],[-x2[0],x3[0],0]]) 
     return x_hat
 
 def cvdata2transmtx(rvec,tvec):
     # Rotation and translation of Camera to ArUco
-    R_temp = np.array([[1,0,0],[0,1,0],[0,0,1]]) 
-    p_temp = np.array([0.3048,0,0.1016 ])
+    R_temp = cv2.Rodrigues(rvec)[0] 
+    p_temp = tvec
     # Find the Rotation and translation of ArUco to Camera
-    R = cv2.Rodrigues(x_hat)[0]
-    p = np.array([0.3048,0,0.1016 ])
-    g = cvdata2transmtx(R,p)
+    R = numpy.linalg.inv(R_temp)[0]
+    p = -p_temp
+    g = np.array([[g00[0],g01[0],g02[0],g03[0]],[g10[0],g11[0],g12[0],g13[0]],[g20[0],g21[0],g22[0],g23[0]],[0,0,0,1]])
     return g, R, p
 
 def transmtx2twist(g):
     # Rotation and translation from g
-    R = cv2.Rodrigues(x_hat)[0]
-    p = np.array([0.3048,0,0.1016 ])
+    R = numpy.hstack([[g00,g01,g02],[g10,g11,g12],[g20,g21,g22]])
+    p = numpy.hstack([[g03],[g13],[g23]])
     # Convert the rotation matrix to rotation vector (including theta)
-    rvec = numpy.linalg.inv(cv2.aruco.estimatePoseSingleMarkers())
+    theta= math.acos((R.trace()-1)/2)
+    rvec = np.hstack([[r21-r12],[r02-r20],[r10-r01]])*(1- 2*math.sin(theta))
     # Find the twist coordinate
-    th = numpy.linalg.norm(rvec)
-    w = np.array([[0,0,1]])
-    v = np.array([[0,0,0]])
+    th = np.linalg.norm(rvec)
+    w = rvec
+    v = 
     return v, w, th
 
 def twist2screw(v,w,th):
     # Convert the twist coordinate to screw motion
-    q = np.array([[0.3048,0,0]])
-    h = 0
+    q = math.cross(w,v)/(numpy.linalg.norm(w))^2
+    h = 
     u = np.array([[0,0,1]])
     M = 1
     return q, h, u, M
